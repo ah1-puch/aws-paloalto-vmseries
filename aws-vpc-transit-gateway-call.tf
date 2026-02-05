b96297d5e0e2a617c3368d4b8c54f3c5c9bd1321
ah1-puch <145569521+ah1-puch@users.noreply.github.com>
2026-02-05 11:23:37 +0200

    Update aws-vpc-transit-gateway-call.tf

--- aws-vpc-transit-gateway-call.tf
+++ aws-vpc-transit-gateway-call.tf
@@ -1,14 +1,14 @@
 module "cust_transit_gateway" {
   source = "git::https://github.com/TechService-Public-Cloud-Products/aws-tf-core-vpc.git?ref=main"
-  # source = "../aws-tf-core-vpc"
   
   vpc = {
     transit_gateway = local.config.network.transit_gateway
   }
-  common = local.config.common
   
   reference = {
     aws_vpc    = module.cust_vpc.aws_vpc
     aws_subnet = module.cust_vpc.aws_subnet
   }
+
+  common = local.config.common
 } 
