e5a6c0295895dc9596b104f1e84d14fcc4b9faea
ah1-puch <145569521+ah1-puch@users.noreply.github.com>
2026-02-05 11:33:39 +0200

    Update aws-vpc-only-call.tf

--- aws-vpc-only-call.tf
+++ aws-vpc-only-call.tf
@@ -1,17 +1,16 @@
 module "cust_vpc" {
   source = "git::https://github.com/GithubOrg/aws-tf-core-vpc.git?ref=main"
-  # source = "../aws-tf-core-vpc"
-  
-  # Only pass VPC and subnet configuration, exclude network_interface and transit_gateway
+
   vpc = {
     vpc         = local.config.network.vpc
     subnet      = local.config.network.subnet
     route_table = local.config.network.route_table
     route       = local.config.network.route
   }
-  common = local.config.common
   
   reference = {
     aws_ec2_transit_gateway = module.cust_transit_gateway.aws_ec2_transit_gateway
   }
+
+  common = local.config.common
 } 
