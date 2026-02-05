27605ab0f3ae7dd368bbf0d6f3844221f2d101df
ah1-puch <145569521+ah1-puch@users.noreply.github.com>
2026-02-05 11:35:33 +0200

    Update aws-compute-ec2-call.tf

--- aws-compute-ec2-call.tf
+++ aws-compute-ec2-call.tf
@@ -1,10 +1,8 @@
 module "cust_ec2" {
   source = "git::https://github.com/GithubOrg/aws-tf-core-ec2.git?ref=main"
-  # source = "../aws-tf-core-ec2"
   
   datasource  = local.config.compute.datasource
   ec2         = local.config.compute.ec2
-  common      = local.config.common
   
   reference = {
     aws_network_interface = module.cust_nic.aws_network_interface
@@ -12,4 +10,6 @@ module "cust_ec2" {
       vmseries_instance_profile = module.cust_iam.aws_iam_instance_profile.vmseries_instance_profile
     }
   }
+
+  common      = local.config.common
 } 
