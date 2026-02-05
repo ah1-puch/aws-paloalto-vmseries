3bfc0b727e3362b50913ef7b8e8b0ddb2a859702
ah1-puch <145569521+ah1-puch@users.noreply.github.com>
2026-02-05 11:32:43 +0200

    Update aws-iam-call.tf

--- aws-iam-call.tf
+++ aws-iam-call.tf
@@ -1,7 +1,6 @@
 # IAM resources for VM Series
 module "cust_iam" {
   source = "git::https://github.com/GithubOrg/aws-tf-core-iam.git?ref=main"
-  # source = "../aws-tf-core-iam"
 
   iam = local.config.iam
 
